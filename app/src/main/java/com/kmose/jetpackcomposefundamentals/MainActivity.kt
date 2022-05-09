65b81a7d2256f7502c54f7469038e879f59d77fc
user <km_413@naver.com>
2022-05-09 16:44:23 +0900

    Modifier And Parameters

--- app/src/main/java/com/kmose/jetpackcomposefundamentals/MainActivity.kt
+++ app/src/main/java/com/kmose/jetpackcomposefundamentals/MainActivity.kt
@@ -3,25 +3,30 @@ package com.kmose.jetpackcomposefundamentals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kmose.jetpackcomposefundamentals.ui.theme.JetpackComposeFundamentalsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeFundamentalsTheme { // [ProjectName]Theme
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android \n\t 여기는 실제 화면")
         }
     }
 }
@@ -29,7 +34,20 @@ class MainActivity : ComponentActivity() {
 // Package level function - allow us to reuse them effectively.
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    // Jetpack Compose Function - like UI Widget in xml
+    Text(
+        text = "Hello $name!",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .padding(10.dp)
+            .border(2.dp, color = Color.Green)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)
+    )
 }
 
 @Preview(showBackground = true)
