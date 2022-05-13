dbd97dab95f8fc80bafe4a2d3cfcc912c83de1fd
user <km_413@naver.com>
2022-05-13 15:33:47 +0900

    Buttons

--- app/src/main/java/com/kmose/jetpackcomposefundamentals/MainActivity.kt
+++ app/src/main/java/com/kmose/jetpackcomposefundamentals/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.kmose.jetpackcomposefundamentals
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -25,13 +28,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kmose.jetpackcomposefundamentals.ui.theme.JetpackComposeFundamentalsTheme
-import androidx.compose.material.Button as ButtonDefaults
+//import androidx.compose.material.Button as ButtonDefaults
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BoxExample3()
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                ButtonDemo()
+            }
         }
     }
 }
@@ -176,7 +185,7 @@ fun BoxExample3() {
             color = Color.White,
             modifier = Modifier.align(Alignment.BottomStart)
         )
-        ButtonDefaults(
+        Button(
             onClick = {},
             colors = ButtonDefaults.textButtonColors(
                 backgroundColor = Color.White,
@@ -189,6 +198,82 @@ fun BoxExample3() {
         ) {
             Text("Back To List")
         }
+    }
+}
+
+@Composable
+fun ButtonDemo() {
+    val context = LocalContext.current
+
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button2", Toast.LENGTH_SHORT).show()
+                     },
+        enabled = false
+    ) {
+        Text("Add To Cart")
+    }
+
+    TextButton(onClick = { // button without a background shape or border
+        Toast.makeText(context, "Clicked on TextButton", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
+
+    OutlinedButton(onClick = { //
+        Toast.makeText(context, "Clicked on OutlineButton", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
+
+    IconButton(onClick = { // like clickable icon
+        Toast.makeText(context, "Clicked on IconButton", Toast.LENGTH_SHORT).show()
+    }) {
+        // 기본적으로 painter로 이미지나 백터이미지를 넣을 수 있다
+        Icon( imageVector = Icons.Filled.Refresh,
+            contentDescription = "Refresh Button",
+            tint = Color.Green,
+            modifier = Modifier.size(80.dp)
+        )
+    }
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    },
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = Color.DarkGray,
+            contentColor =  Color.White
+        )
+    ) {
+        Text("Add To Cart",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    },
+        shape = CutCornerShape(10.dp),
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = Color.DarkGray,
+            contentColor =  Color.White
+        )
+    ) {
+        Text("Add To Cart",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.padding(5.dp)
+        )
     }
 }
 
